f1a5bf9e6f71c7d16e3d87e3beb27c25ec19c97a
Raki6655 <alannpn75@gmail.com>
2025-05-10 14:01:22 +0530

    update:file structure modification

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 "use client";
-import { IPhoneFrame } from "./components/IphoneFrame";
-import { ScreenTwo } from "./components/ScreenTwo";
-import { ScreenThree } from "./components/ScreenThree";
-import { ScreenOne } from "./components/ScreenOne";
-import { Fader } from "./components/Fader";
-import { useAnimation } from "./hooks/useAnimation";
+
+import { useAnimation } from "@/hooks/useAnimation";
+import { ScreenOne } from "@/components/ScreenOne";
+import { ScreenTwo } from "@/components/ScreenTwo";
+import { ScreenThree } from "@/components/ScreenThree";
+import { IPhoneFrame } from "@/components/IphoneFrame";
+
 export default function Home() {
 	const { animation, startFirstTimeline } = useAnimation();
 	return (
